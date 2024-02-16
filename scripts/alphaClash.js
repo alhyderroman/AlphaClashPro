7afd9a93e2a343f1987534885fb63e81372f64f3
alhyderroman <alhyderroman807@yahoo.com>
2024-02-16 22:05:14 +0600

    message

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -8,10 +8,54 @@
 
 // }
 
+function handlekeyboardButtonPress(event){
+    const playerPressed=event.key;
+
+    // the expected key to press
+const currentAlphabetElement=document.getElementById('current-alphabet');
+const currentAlphabet=currentAlphabetElement.innerText;
+const expectedAlphabet=currentAlphabet.toLowerCase();
+console.log(playerPressed,expectedAlphabet);
+//check matched or not
+if(playerPressed===expectedAlphabet){
+    console.log('You got a point.');
+
+//     //update score
+   //const currentScoreElement=document.getElementById('current-score');
+//     const currentScoreText=currentScoreElement.innerText;
+//    const currentScore=parseInt(currentScoreText);
+const currentScore=getTextElementValueById('current-score');
+   const newScore=currentScore+1;
+//    // set updated score
+ setTextElementValueById('current-score',newScore);
+   
+
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+
+}
+else{
+    // const currentLifeElement=document.getElementById('current-life');
+    // const initialLifeText=currentLifeElement.innerText;
+    // const currentLife=parseInt(initialLifeText);
+    const currentLife=getTextElementValueById('current-life');
+    const newLife=currentLife-1;
+    setTextElementValueById('current-life',newLife);
+}
+}
+//capture keyboard key pressed
+document.addEventListener('keyup',handlekeyboardButtonPress);
+
+
+
+    
+
+
+
 function continueGame(){
     //generate a random alphabet.
     const alphabet=getARandomAlphabet();
-    console.log('Your random alphabet='+alphabet);
+    // console.log('Your random alphabet='+alphabet);
     //set randomly generated alphabet to the screen
     const currentAlphabet=document.getElementById('current-alphabet');
     currentAlphabet.innerText=alphabet;
